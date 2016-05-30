03f982e757f1696a747636541ed78ab8eae65f69
doctorclo <doctorclo2011@gmail.com>
2016-05-31 01:30:33 +0300

    FInal2 STL TO BOOST

--- p5/shell.cpp
+++ p5/shell.cpp
@@ -83,66 +83,6 @@ int main()
     }
     return 0;
 }
-void parse(char *input, char **&cmd,std::list<std::string> &res)
-{
-    cmd = (char **) malloc(sizeof(*cmd));
-    cmd[0] = (char *) malloc(sizeof(**cmd));
-
-    int cap = 1, pos = 0, posi = 0, n = int(strlen(input)), currcap = 1, qopen = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (input[i] == '\'') {
-            qopen ^= 1;
-            continue;
-        }
-
-        if (!qopen && posi > 0 && (isspace(input[i]) || (ismanaging(input[i]) ^ ismanaging(input[i - 1])))) {
-            pos++;
-            posi = 0;
-        }
-        
-        if (pos + 1 == cap) {
-            cmd = (char **) realloc(cmd, sizeof(*cmd) * cap * 2);
-
-            for (int j = cap; j < cap * 2; j++) {
-                cmd[j] = NULL;
-            }
-            cap *= 2;
-        }
-
-        if (cmd[pos] == NULL) {
-            cmd[pos] = (char *) malloc(sizeof(**cmd));
-            cmd[pos][posi] = 0;
-            currcap = 1;
-        }
-
-        if (posi + 1 == currcap) {
-            cmd[pos] = (char *) realloc(cmd[pos], sizeof(**cmd) * currcap * 2);
-
-            for (int j = currcap; j < currcap * 2; j++) {
-                cmd[pos][j] = 0;
-            }
-            currcap *= 2;
-        }
-        
-        if (qopen || !isspace(input[i])) {
-            cmd[pos][posi] = input[i];
-            posi++;
-        }
-    }
-
-    if (posi == 0) {
-        free(cmd[pos]);
-        cmd[pos] = NULL;
-    }
-    res.clear();
-    for (int i=0;i<pos;i++)
-    {
-        std::cout<<cmd[i]<<std::endl;
-        res.push_back(std::string(cmd[i]));
-    }
-}
-
 void parse_cmd(std::list<std::string> &cmds,const std::string &_s)
 {
     std::string command;
